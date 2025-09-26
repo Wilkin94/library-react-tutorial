75f4545efb82dadce631b3e878048c95b90feb0a
wilkin <wilkinanthony94@gmail.com>
2025-09-26 05:32:34 -0500

    library

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+Library.add(faBars)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
